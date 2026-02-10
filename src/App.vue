18a326649c7ef348a013ea098c2e1a6950e0d765
Youcef <youcefcopy@gmail.com>
2026-02-10 15:36:50 +0100

    navbar created && preparing for movies objects

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
-<script setup></script>
+<script setup>
+import MyNav from './components/MyNav.vue'
+</script>
 
 <template>
+  <MyNav />
   <h1>You did it!</h1>
 </template>
 
---
+++ src/components/MyNav.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<template>
+  <nav>
+    <h2>Top Movies</h2>
+    <i class="fa-solid fa-magnifying-glass"></i>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 2rem;
+}
+</style>
