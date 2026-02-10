dfdfb0763b06e4533239e265a91ad8d427bb1142
Youcef <youcefcopy@gmail.com>
2026-02-10 15:46:22 +0100

    definign the props in the movie component

--- src/components/MovieC.vue
+++ src/components/MovieC.vue
@@ -1,10 +1,24 @@
 <script setup>
+import { defineProps } from 'vue'
 import movies from '@/data/movies.js'
 console.log(movies)
+
+// we need props heree
+defineProps({
+  title: {
+    type: String,
+  },
+  img: {
+    type: URL,
+  },
+})
 </script>
 
 <template>
-  <div class="movie"></div>
+  <div class="movie">
+    <img :src="img" :alt="title" />
+    <h2>{{ title }}</h2>
+  </div>
   right movie
 </template>
 
