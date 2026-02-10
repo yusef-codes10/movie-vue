0aa6c38445f9ca9207c1c9f1af0aad957fbdfa8c
Youcef <youcefcopy@gmail.com>
2026-02-10 15:48:50 +0100

    v-for + props to load movies

--- src/components/MoviesContainer.vue
+++ src/components/MoviesContainer.vue
@@ -1,10 +1,13 @@
 <script setup>
 import MovieC from './MovieC.vue'
+import movies from '@/data/movies.js'
+console.log(movies)
 </script>
 
 <template>
-  pass
-  <MovieC />
+  <div class="movies-container" v-for="movie in movies" :key="movie.id">
+    <MovieC :img="movie.poster" :title="movies.title" />
+  </div>
 </template>
 
 <style scoped></style>
